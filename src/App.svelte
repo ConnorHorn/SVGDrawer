029a54f9a614789e2d709abab9246cc981641a87
ConnorHorn <connorhornet@gmail.com>
2023-07-03 18:03:11 -0400

    Fixed issue with not drawing entire path. Added Drawing.

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,18 @@
 <script>
     import Drawing from "./Drawing.svelte";
+    import {
+        circle,
+        cubeFlip,
+        flame,
+        loadBar, nathanGandalf,
+        spikyBall, threeLines,
+        benchmark,
+        porterHand
+
+    } from "./drawings";
+    let drawingList = [
+        $porterHand
+    ];
 </script>
 
-<Drawing/>
+<Drawing drawingList={drawingList}/>
